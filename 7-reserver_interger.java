4f713fbd76d105a1cb633c8fb0c9b11cd2da7365
Chun-Kai Kao <cg.workst@gmail.com>
2020-09-26 12:58:29 +0800

    leetcode ans

--- 7-reserver_interger.java
+++ 7-reserver_interger.java
@@ -1,57 +1,20 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println(reverse(-2147483648));
+        System.out.println(reverse(-1463847412));
     }
 
     public static int reverse(int x) {
-        boolean negative = false;
-        long lx;
-        if (x == -2147483648) {
-            negative = true;
-            lx = 2147483648L;
-        } else if (x < 0){
-            negative = true;
-            lx = -x;
-        } else {
-            lx = x;
+        int ans = 0;
+        while (x != 0){
+            int pop = x%10;
+            x /= 10;
+
+            System.out.println(pop);
+            if (ans > Integer.MAX_VALUE/10 || ans == Integer.MAX_VALUE/10 && pop > 7) return 0;
+            if (ans < Integer.MIN_VALUE/10 || ans == Integer.MIN_VALUE/10 && pop < -8) return 0;
+            ans *= 10;
+            ans += pop;
         }
-        
-        String src = Long.toString(lx);
-        int digits = src.length();
-
-        // only one digit, return it.
-        if (digits == 1) {
-            return x;
-        }
-
-        int maxDigit = digits - 1;
-
-        boolean hasMaxDigit = false;
-
-        while (!hasMaxDigit && maxDigit >= 1) {
-            char leftD = src.charAt(maxDigit - 1);
-            char rightD = src.charAt(maxDigit);
-            if (leftD != '0' && rightD == '0') {
-                hasMaxDigit = true;
-                maxDigit--;
-            } else if (leftD != '0' && rightD != '0') {
-                hasMaxDigit = true;
-            } else if (leftD == '0' && rightD != '0') {
-                hasMaxDigit = true;
-            } else if (leftD == '0' && rightD == '0') {
-                maxDigit--;
-            }
-        }
-
-        StringBuilder outStrBdr = new StringBuilder(src.substring(0, maxDigit + 1));
-        String outStr = outStrBdr.reverse().toString();
-
-        long out = Long.parseLong(outStr);
-
-        if (Math.abs(out) > Integer.MAX_VALUE){
-            return 0;
-        } else {
-            return negative ? (int) -out: (int) out;
-        }
+        return ans;
     }
 }
